622b9b734dc3b79b9d8256389b0ce78b0c5c8d43
leevik <47596878+leevik@users.noreply.github.com>
2022-09-28 15:20:43 +0300

    app built

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
+app.use(express.static('build'))
 
 morgan("tiny")
 
